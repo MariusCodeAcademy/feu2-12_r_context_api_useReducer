a814c4e846bd0aea184bd68e1863adb16d6b8158
Marius <marius@example.com>
2022-12-06 08:47:06 +0200

    i router, Page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import UserOnlyPage from './pages/UserOnlyPage';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>React</h1>
+    <div className='App container'>
+      <Switch>
+        <Route path={'/user-page'}>
+          <UserOnlyPage />
+        </Route>
+        <Route path={'/login'}>
+          <LoginPage />
+        </Route>
+        <Route path={'/'} exact>
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
