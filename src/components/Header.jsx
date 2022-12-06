799f45ca9e4401459bcf65746d66d1e116105a41
Marius <marius@example.com>
2022-12-06 10:04:03 +0200

    redirect, Dynamic header uzd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header(props) {
+  const isUserLoggedIn = true;
+  const userEmail = 'jane@doe.com';
+
   return (
     <header className='main-header'>
       <nav>
         <NavLink className='nav-link' to='/' exact>
           Home
         </NavLink>
-        <NavLink className='nav-link' to='/login'>
-          Login
-        </NavLink>
         <NavLink className='nav-link' to='/user-page'>
           Users Page
         </NavLink>
+        <NavLink className='nav-link' to='/login'>
+          Login
+        </NavLink>
+        <Link className='nav-link' to={'/logout'}>
+          Logout
+        </Link>
+        {/* jei useris prisilogines, tai rodom louguot */}
+        {/* jei useris prisilogines, tai userio email */}
+        {/* jei neprisilogines login */}
       </nav>
     </header>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
+import { useHistory } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 
 function LoginPage(props) {
+  const history = useHistory();
   const handleLogin = (newLoginObj) => {
     console.log('handleLogin in LoginPage', newLoginObj);
     // if login success we redirect to userOnly page
@@ -10,6 +12,7 @@ function LoginPage(props) {
       // login success
       console.log('login success');
       // redirect
+      history.push('/user-page');
     } else {
       // login fails
       console.log('login fails');
